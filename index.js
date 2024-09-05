a796720880ad05e7e36f7a9d312e7bdbed3db557
shanky <thakurdeepanshu420@gmail.com>
2024-09-05 17:05:02 +0530

    encyclopedia made

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const apiKey = process.env.API_KEY;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 app.get("/", (req, res) => {
-  res.send("please use the endpoint       https://sih-2024-six.vercel.app/suggestions?location={your_location_here}       followed by your district name");
+  res.send(
+    "please use the endpoint       https://sih-2024-six.vercel.app/suggestions?location={your_location_here}       followed by your district name"
+  );
 });
 
 app.get("/suggestions", async (req, res) => {
-
   const genAI = new GoogleGenerativeAI(apiKey);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
@@ -19,6 +20,28 @@ app.get("/suggestions", async (req, res) => {
 
   const result = await model.generateContent(prompt);
   res.send(JSON.stringify(result.response));
+});
+
+app.get("/encyclopedia", async (req, res) => {
+  try {
+    let PROJECT_ID = "i1k9pkau";
+    let DATASET = "production";
+    let QUERY = encodeURIComponent(
+      '*[_type == "plant"]{_id,name,"image":image.asset->url,description,uses,planting,cautions}'
+    );
+    let QUERY2 = encodeURIComponent('*[_type == "plant"]');
+
+    // Compose the URL for your project's endpoint and add the query
+    let URL = `https://${PROJECT_ID}.api.sanity.io/v2022-03-07/data/query/${DATASET}?query=${QUERY2}`;
+
+    fetch(URL)
+      .then((res) => res.json())
+      .then(({ result }) => {
+        res.send(result);
+      });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
